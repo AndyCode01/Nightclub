1610cf1be4a962162e377bd9fb11f4cb249d82f5
MorasDeFresa <bencito10@gmail.com>
2024-03-12 21:36:41 -0500

    fix(Model): fix Foreign key and Key

--- Models/Artist.cs
+++ Models/Artist.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Nightclub.Models
 {
     public class Artist
     {
-        [Key] public required int Id_artist { get; set; }
+        [Key][DatabaseGenerated(DatabaseGeneratedOption.Identity)] public required int Id_artist { get; set; }
         public required string name_artist { get; set; }
         public required string surname_artist { get; set; }
         public required string document_number { get; set; }
--- Models/Client.cs
+++ Models/Client.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Nightclub.Models
 {
     public class Client
     {
-        [Key] public required int Id_client {get; set;}
-        public required string name_client {get; set;}
-        public required string surname_client {get; set;}
-        public required string document_number {get; set;}
-        public Type_document document_ { get; set; }
-        public List<Ticket> ticket{ get; set; }
+        [Key][DatabaseGenerated(DatabaseGeneratedOption.Identity)] public required int Id_client { get; set; }
+        public required string name_client { get; set; }
+        public required string surname_client { get; set; }
+        public required string document_number { get; set; }
+        public required int type_documentId { get; set; }
+        [ForeignKey("type_documentId")] public Type_document type_Document { get; set; }
+        public List<Ticket> ticket { get; set; }
     }
 }
